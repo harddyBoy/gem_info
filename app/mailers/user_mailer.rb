58a28829ef4aabdbd60fa3fe7c3c958f7d16c346
harddyBoy <bqkapko@gmail.com>
2019-05-15 12:19:30 +0300

    done with devise stuff

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+	default from: 'geminfo@gmail.com'
+
+	def welcome_email
+		@user = params[:user]
+		@url  = 'http://geminfo.com/login'
+		mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+	end
+end
